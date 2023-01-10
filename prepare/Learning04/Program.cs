04b0c5aa59654f874c9344872b718506d8719f01
Stephen Beckstead <spotterbeckstead@gmail.com>
2023-01-09 18:52:46 -0800

    lets gooooo

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        List<int> sblist = new List<int>(); 
+
+        int sbnumbers = -1; 
+
+        while (sbnumbers != 0) {
+        Console.WriteLine("Enter Number");
+        string sbrandom = Console.ReadLine();
+        
+        sbnumbers = int.Parse(sbrandom); 
+
+        sblist.Add(sbnumbers); 
+
+
+        }
+
+    int sbsum = 0; 
+     foreach (int i in sblist) {
+        sbsum += i; 
+     }
+     Console.WriteLine($"The sum is: {sbsum}");
+
+     int sbaverage = sbsum / sblist.Count;
+     Console.WriteLine($"The average is {sbaverage}");
+
+     int sblargest = 0; 
+
+     foreach (int sbnumber in sblist) {
+        if(sbnumber > sblargest) {
+            sblargest = sbnumber; 
+        }
+
+    }
+    Console.WriteLine($"The largest number is {sblargest}");
+
     }
 }
